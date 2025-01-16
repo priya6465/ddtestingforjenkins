dd2f4d7567046f050fc1e33e4f19022fb5c4acae
priyanka <p.priyapatil579@gmail.com>
2025-01-16 11:01:56 +0530

    added 8 t

--- src/test/java/com/qa/Test/tooldipdemo.java
+++ src/test/java/com/qa/Test/tooldipdemo.java
@@ -28,11 +28,6 @@ public class tooldipdemo {
 	@Test
 	public void test5() {
 		System.out.println("this is test 5");
-
-	}
-	@Test
-	public void test6() {
-		System.out.println("this is test 6");
 	}
 
 	@Test
@@ -41,7 +36,12 @@ public class tooldipdemo {
 	}
 
 	@Test
-	public void test5() {
+	public void test7() {
+		System.out.println("this is test 7");
+	}
+
+	@Test
+	public void test8() {
 		System.out.println("this is test 7");
 	}
 
